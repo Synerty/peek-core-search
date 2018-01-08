944da1e1447be152362d6e18c227c321a1b95987
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-01-08 20:14:25 +1000

    Updated to version 0.8.0

--- peek_plugin_unified_search/__init__.py
+++ peek_plugin_unified_search/__init__.py
@@ -4,7 +4,7 @@ from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHo
 from peek_plugin_base.agent.PluginAgentEntryHookABC import PluginAgentEntryHookABC
 from typing import Type
 
-__version__ = '0.7.9.3'
+__version__ = '0.8.0'
 
 
 def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:
