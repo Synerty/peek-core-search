6a0e13fa9fd10d7403c099df2bb9a6e42f3f9b8c
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-07-06 08:17:18 +1000

    Updated to version 1.0.0.4

--- peek_plugin_search/__init__.py
+++ peek_plugin_search/__init__.py
@@ -4,7 +4,7 @@ from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHo
 from peek_plugin_base.worker.PluginWorkerEntryHookABC import PluginWorkerEntryHookABC
 from typing import Type
 
-__version__ = '1.0.0.3'
+__version__ = '1.0.0.4'
 
 
 def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:
