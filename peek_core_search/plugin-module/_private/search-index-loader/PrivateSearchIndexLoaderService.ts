0ff0b43b0412b68ddd86ad7acc3fea82fa238f7c
Jarrod Chesney <jarrod.chesney@synerty.com>
2023-02-26 14:22:28 +1000

    PEEK-1904 Removed vortexOnline -> askServerForUpdates subscription

--- peek_core_search/plugin-module/_private/search-index-loader/PrivateSearchIndexLoaderService.ts
+++ peek_core_search/plugin-module/_private/search-index-loader/PrivateSearchIndexLoaderService.ts
@@ -268,12 +268,6 @@ export class PrivateSearchIndexLoaderService extends NgLifeCycleEvents {
             .subscribe((payloadEnvelope: PayloadEnvelope) => {
                 this.processChunksFromServer(payloadEnvelope);
             });
-
-        // If the vortex service comes back online, update the watch grids.
-        this.vortexStatusService.isOnline
-            .pipe(filter((isOnline) => isOnline == true))
-            .pipe(takeUntil(this.onDestroyEvent))
-            .subscribe(() => this.askServerForUpdates());
     }
 
     private areWeTalkingToTheServer(): boolean {
